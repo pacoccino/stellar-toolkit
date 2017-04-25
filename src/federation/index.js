d1a9d156c12673d23f21a836f1f0d232472ce176
Paco <pakokrew@gmail.com>
2017-04-25 15:41:14 +0200

    WIlson and federation doc

--- src/federation/index.js
+++ src/federation/index.js
@@ -2,12 +2,21 @@ const { Keypair } = require('stellar-sdk');
 const request = require('../helpers/request');
 const { sign } = require('../stellar/DataManager');
 
-let federationUrl = "https://stellar-wilson.herokuapp.com/federation";
+let federationUrl = "https://api.willet.io/federation";
 
+/**
+ * @description Set URL of the federation server
+ * @param {String} url  - Url of the federation server
+ */
 function setUrl(url) {
   federationUrl = url;
 }
 
+/**
+ * Resolve account ID from username
+ * @param {String} stellar_address - Federation name
+ * @returns {Object} FederationResult
+ */
 function federationResolve(stellar_address) {
   return request({
     url: federationUrl,
@@ -18,6 +27,11 @@ function federationResolve(stellar_address) {
   });
 }
 
+/**
+ * @description Resolve username from account ID
+ * @param {String} account_id - Stellar account ID
+ * @returns {Object} FederationResult - Stellar account informations if found on the server
+ */
 function federationReverse(account_id) {
   return request({
     url: federationUrl,
@@ -28,6 +42,13 @@ function federationReverse(account_id) {
   });
 }
 
+/**
+ * @description Returns the keypair of the account if the seed is stored encrypted with a password in account data
+ * @param {Object} data
+ * @param {String} data.q - Stellar address
+ * @param {String} data.password - Password
+ * @returns {Object} keypair - Seed + Account ID
+ */
 function federationKeypair({ q, password }) {
   return request({
     url: federationUrl,
@@ -39,6 +60,11 @@ function federationKeypair({ q, password }) {
   });
 }
 
+/**
+ * @description Create a stellar account and register it on the federation server
+ * @param stellar_address - Federation address to register
+ * @returns {Keypair} keypair - Newly created account's keypair
+ */
 function federationCreate(stellar_address) {
   const keypair = Keypair.random();
   const body = {
@@ -56,6 +82,12 @@ function federationCreate(stellar_address) {
   }).then(() => keypair);
 }
 
+/**
+ * @description Register an existing stellar account on the federation server
+ * @param stellar_address - Federation name to register
+ * @param keypair - Keypair of the existing stellar account to register (to sign the message)
+ * @returns {*}
+ */
 function federationRegister({ stellar_address, keypair }) {
   const body = {
     stellar_address,
@@ -73,6 +105,12 @@ function federationRegister({ stellar_address, keypair }) {
   });
 }
 
+/**
+ * @description Removes an existing stellar account on the federation server
+ * @param stellar_address - Federation name to register
+ * @param keypair - Keypair of the existing stellar account to register (to sign the message)
+ * @returns {*}
+ */
 function federationDelete({ stellar_address, keypair }) {
   const body = {
     stellar_address,
